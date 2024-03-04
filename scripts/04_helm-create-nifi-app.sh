c6af90a3ac6eaa6ca7a53ced9db173cb544c15ac
Adam Cubel <adam.cubel@tactware.net>
2024-03-04 14:56:55 -0600

    more changes to 04

--- scripts/04_helm-create-nifi-app.sh
+++ scripts/04_helm-create-nifi-app.sh
@@ -37,18 +37,47 @@ else
     curl -o iam_policy.json https://raw.githubusercontent.com/kubernetes-sigs/aws-load-balancer-controller/v2.5.4/docs/install/iam_policy_us-gov.json 
 fi
 
-aws iam create-policy \
-    --policy-name AWSLoadBalancerControllerIAMPolicy \
-    --policy-document file://"iam_policy.json"
+aws iam create-policy --policy-name AWSLoadBalancerControllerIAMPolicy --policy-document file://./iam_policy.json
 
 oidc_id=$(aws eks describe-cluster --name "$cluster_name" --query "cluster.identity.oidc.issuer" --output text | cut -d '/' -f 5)
 
-# Troubleshooting step to ensure OIDC ID is created with the cluster. If one is not created,
-# follow the instructions here: https://docs.aws.amazon.com/eks/latest/userguide/enable-iam-roles-for-service-accounts.html
-# aws iam list-open-id-connect-providers | grep $oidc_id | cut -d "/" -f4
+# # Troubleshooting step to ensure OIDC ID is created with the cluster. If one is not created,
+# # follow the instructions here: https://docs.aws.amazon.com/eks/latest/userguide/enable-iam-roles-for-service-accounts.html
+# # aws iam list-open-id-connect-providers | grep $oidc_id | cut -d "/" -f4
 
 account_id=$(aws sts get-caller-identity | jq --raw-output '.Account')
 oidc_arn=$(aws iam list-open-id-connect-providers | jq --raw-output '.OpenIDConnectProviderList[0].Arn')
+
+cat > vpc-cni-trust-policy.json <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Principal": {
+                "Federated": "$oidc_arn"
+            },
+            "Action": "sts:AssumeRoleWithWebIdentity",
+            "Condition": {
+                "StringEquals": {
+                    "oidc.eks.$cluster_region.amazonaws.com/id/$oidc_id:aud": "sts.amazonaws.com",
+                    "oidc.eks.$cluster_region.amazonaws.com/id/$oidc_id:sub": "system:serviceaccount:kube-system:aws-node"
+                }
+            }
+        }
+    ]
+}
+EOF
+
+aws iam create-role --role-name AmazonEKSVPCCNIRole --assume-role-policy-document file://./vpc-cni-trust-policy.json
+aws iam attach-role-policy --policy-arn arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy --role-name AmazonEKSVPCCNIRole
+kubectl annotate serviceaccount -n kube-system aws-node eks.amazonaws.com/role-arn=arn:aws:iam::$account_id:role/AmazonEKSVPCCNIRole
+aws eks create-addon --cluster-name my-cluster --addon-name vpc-cni --addon-version v1.16.2-eksbuild.1 --service-account-role-arn arn:aws:iam::$account_id:role/AmazonEKSVPCCNIRole
+
+eks_build=$(kubectl describe daemonset aws-node --namespace kube-system | grep amazon-k8s-cni: | cut -d : -f 3)
+curl -O https://raw.githubusercontent.com/aws/amazon-vpc-cni-k8s/v1.16.2/config/master/aws-k8s-cni.yaml
+kubectl apply -f aws-k8s-cni.yaml
+
 cat > load-balancer-role-trust-policy.json <<EOF
 {
     "Version": "2012-10-17",
@@ -70,13 +99,9 @@ cat > load-balancer-role-trust-policy.json <<EOF
 }
 EOF
 
-aws iam create-role \
-  --role-name AmazonEKSLoadBalancerControllerRole \
-  --assume-role-policy-document file://"load-balancer-role-trust-policy.json"
+aws iam create-role --role-name AmazonEKSLoadBalancerControllerRole --assume-role-policy-document file://./load-balancer-role-trust-policy.json
 
-aws iam attach-role-policy \
-  --policy-arn arn:aws:iam::$account_id:policy/AWSLoadBalancerControllerIAMPolicy \
-  --role-name AmazonEKSLoadBalancerControllerRole
+aws iam attach-role-policy --policy-arn arn:aws:iam::$account_id:policy/AWSLoadBalancerControllerIAMPolicy --role-name AmazonEKSLoadBalancerControllerRole
 
 if [[ "$env" == "low" ]]; then
 cat > aws-load-balancer-controller-service-account.yaml <<EOF
@@ -109,21 +134,18 @@ fi
 kubectl apply -f aws-load-balancer-controller-service-account.yaml
 
 if [[ "$env" == "low" ]]; then
-# # TODO: Parameterize the version of the load balancer
-  sudo docker pull public.ecr.aws/eks/aws-load-balancer-controller:latest
+  # TODO: Parameterize the version of the load balancer
   helm repo add eks https://aws.github.io/eks-charts
   helm repo update eks
 fi
 
-# TODO: If this is an upgrade, 
-# kubectl apply -k "github.com/aws/eks-charts/stable/aws-load-balancer-controller/crds?ref=master"
-# Then run helm 'upgrade' rather than helm 'install'
+# run helm 'upgrade' rather than helm 'install' if this is an upgrade
 if [[ "$type" == "install" ]]; then
     helm install aws-load-balancer-controller eks/aws-load-balancer-controller \
     -n kube-system \
     --set region=$cluster_region \
     --set vpcId=$cluster_vpc \
-    --set clusterName=my-cluster \
+    --set clusterName=$cluster_name \
     --set serviceAccount.create=false \
     --set serviceAccount.name=aws-load-balancer-controller 
 else
@@ -133,10 +155,75 @@ else
     -n kube-system \
     --set region=$cluster_region \
     --set vpcId=$cluster_vpc \
-    --set clusterName=my-cluster \
+    --set clusterName=$cluster_name \
     --set serviceAccount.create=false \
     --set serviceAccount.name=aws-load-balancer-controller 
 fi
+
 # Troubleshooting command to see if/that controller is installed
 # kubectl get deployment -n kube-system aws-load-balancer-controller
+
+cat <<EOF > trust-policy.json
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Federated": "$oidc_arn"
+      },
+      "Action": "sts:AssumeRoleWithWebIdentity",
+      "Condition": {
+        "StringEquals": {
+          "oidc.eks.$cluster_region.amazonaws.com/id/$oidc_id:aud": "sts.amazonaws.com",
+          "oidc.eks.$cluster_region.amazonaws.com/id/$oidc_id:sub": "system:serviceaccount:kube-system:ebs-csi-controller-sa"
+        }
+      }
+    }
+  ]
+}
+EOF
+
+aws iam create-role --role-name AmazonEKS_EBS_CSI_DriverRole --assume-role-policy-document file://./trust-policy.json
+
+aws iam attach-role-policy --policy-arn arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy --role-name AmazonEKS_EBS_CSI_DriverRole
+
+aws eks create-addon \
+ --cluster-name $cluster_name \
+ --addon-name aws-ebs-csi-driver \
+ --service-account-role-arn arn:aws:iam::$account_id:role/AmazonEKS_EBS_CSI_DriverRole
+
+# aws eks describe-addon-versions --addon-name aws-ebs-csi-driver
+
+# Create the AWS EBS Storage Class
+cat <<EOF > storage-class.yaml
+kind: StorageClass
+apiVersion: storage.k8s.io/v1
+metadata:
+  name: aws-pg-sc
+  annotations:
+    storageclass.kubernetes.io/is-default-class: "true"
+provisioner: kubernetes.io/aws-ebs
+parameters:
+  type: gp2
+  fsType: ext4 
+EOF
+kubectl create -f storage-class.yaml
+
+# Remove the original default storage class so pods can make Persistent Volume Claims
+kubectl patch storageclass gp2 -p '{"metadata": {"annotations":{"storageclass.kubernetes.io/is-default-class":"false"}}}'
+
+# Create a certificate to upload into AWS Certificate Store
+# https://gist.github.com/taoyuan/39d9bc24bafc8cc45663683eae36eb1a
+
+# pushd ../helm/nifi
+# helm repo add cetic https://cetic.github.io/helm-charts
+# helm repo add bitnami https://charts.bitnami.com/bitnami
+# helm repo add dysnix https://dysnix.github.io/charts/
+# helm repo update
+
+# helm install -f values.yaml nifi-sisyphus cetic/nifi
+# zarf init
+# popd
+
 set +x

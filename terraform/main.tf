39379ffe551c9a2e5f23611e3b9fab709be06df0
Cloud User <ec2-user@ip-10-100-128-172.us-gov-west-1.compute.internal>
2024-03-12 11:02:27 +0000

    changes for zc

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,38 +1,35 @@
-# Copyright (c) HashiCorp, Inc.
-# SPDX-License-Identifier: MPL-2.0
-
 provider "aws" {
   region = var.region
 }
-
+ 
 data "aws_caller_identity" "current" {}
-
+ 
 data "aws_vpc" "cluster_vpc" {
   id = var.vpc_id
 }
-
+ 
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "19.15.3"
   cluster_name = var.eks_cluster_name
   cluster_version = var.eks_cluster_version
-
-
+  enable_irsa = true 
+ 
   # create_cni_ipv6_iam_policy = var.eks_create_cni_ipv6_iam_policy
   create_iam_role  = var.eks_create_iam_role
   iam_role_arn = var.eks_create_iam_role == true ? null : var.eks_role_arn
-
+ 
   create_kms_key = var.eks_create_kms_key
   cluster_encryption_config = {
     provider_key_arn = var.eks_create_kms_key == true ? null : var.eks_kms_key_arn
     resources = ["secrets"]
   }
-
+ 
   create_cluster_security_group = var.eks_create_cluster_security_group
   cluster_security_group_id = var.eks_create_cluster_security_group == true ? null : var.eks_security_group_id
-  create_node_security_group = var.eks_create_node_security_group 
+  create_node_security_group = var.eks_create_node_security_group
   node_security_group_id = var.eks_create_cluster_security_group == true ? null : var.eks_node_security_group_id
-
+ 
   vpc_id                         = data.aws_vpc.cluster_vpc.id
   subnet_ids                     = var.eks_subnet_ids
   cluster_endpoint_public_access = false
@@ -41,14 +38,37 @@ module "eks" {
   eks_managed_node_group_defaults = {
     ami_type = "AL2_x86_64"
   }
-
+ 
+  node_security_group_additional_rules = {
+    ingress_nodes_from_vpc = {
+          protocol    = "tcp"
+          from_port   = 443
+          to_port     = 443
+          type        = "ingress"
+          cidr_blocks = var.eks_sg_additional_rules_cidr
+    }
+  }
+ 
+  cluster_security_group_additional_rules = {
+    ingress_cluster_from_vpc = {
+          protocol    = "tcp"
+          from_port   = 443
+          to_port     = 443
+          type        = "ingress"
+          cidr_blocks = var.eks_sg_additional_rules_cidr
+    }
+  }
+ 
   eks_managed_node_groups = {
     for i in range(0, var.eks_number_of_node_groups) : "node_${i}" => {
       name = "node-group-${i}"
-      instance_types = var.eks_node_group_instance_types     
+      create_iam_role = var.eks_create_node_group_iam_role
+      iam_role_arn = var.eks_create_node_group_iam_role == true ? null : var.eks_node_group_iam_arn
+      instance_types = var.eks_node_group_instance_types    
       min_size     = var.eks_node_group_min_size
       max_size     = var.eks_node_group_max_size
       desired_size = var.eks_node_group_desired_size
+      # subnet_ids = [var.eks_subnet_ids[i]]
     }
   }
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -3,106 +3,123 @@ variable "region" {
   type        = string
   default     = "us-east-1"
 }
-
+ 
 variable "vpc_id" {
   description = "ID of the VPC to deploy the cluster"
   type = string
   default = ""
 }
-
+ 
 variable "eks_subnet_ids" {
   description = "List of subnet IDs to use when deploying the cluster"
   default = []
 }
-
+ 
 variable "eks_cluster_name" {
   description = "name for cluster"
   type = string
   default = "playground"
 }
-
+ 
 variable "eks_cluster_version" {
   description = "Kubernetes `<major>.<minor>` version to use for the EKS cluster (i.e.: `1.27`)"
   type = string
   default = "1.29"
 }
-
+ 
 variable "eks_node_group_instance_types" {
   description = "collection of instance types for EC2 resources deployed in this EKS module"
   default = ["t3.medium"]
 }
-
+ 
 variable "eks_node_group_min_size" {
   description = "minimum number of nodes in the node group"
   type = number
   default = 1
 }
-
+ 
 variable "eks_node_group_max_size" {
   description = "maximum number of nodes in the node group"
   type = number
   default = 4
 }
-
+ 
 variable "eks_node_group_desired_size" {
   description = "desired number of nodes in the node group"
   type = number
   default = 2
 }
-
+ 
 variable "eks_create_iam_role" {
   description = "boolean to create iam role"
   type = bool
   default = false
 }
-
+ 
 variable "eks_role_arn" {
   description = "ARN of the role to use for the EKS cluster"
   type = string
   default = ""
 }
-
+ 
 variable "eks_create_kms_key" {
   description = "boolean to create KMS key EKS will use for encryption"
   type = bool
   default = false
 }
-
+ 
 variable "eks_kms_key_arn" {
   description = "ARN for KMS Key to use with EKS"
   type = string
   default = ""
 }
-
+ 
 variable "eks_create_cluster_security_group" {
   description = "boolean to create cluster security group"
   type = bool
   default = false
 }
-
+ 
 variable "eks_security_group_id" {
   description = "ID of Security Group to use when deploying EKS"
   type = string
   default = "tf-eks-sg"
 }
-
+ 
 variable "eks_create_node_security_group" {
   description = "boolean to create cluster node security group"
   type = bool
   default = false
 }
-
+ 
 variable "eks_node_security_group_id" {
   description = "ID of Security Group to use for nodes when deploying EKS"
   default = "tf-eks-node-sg"
 }
-
+ 
 variable "eks_number_of_node_groups" {
   type = number
   default = 2
 }
-
+ 
 variable "tags" {
   description = "collection of tags used for resources deployed in this module"
   default = {}
+}
+ 
+variable "eks_create_node_group_iam_role" {
+  description = ""
+  default = true
+  type = bool
+}
+ 
+variable "eks_node_group_iam_arn" {
+  description = ""
+  default = ""
+  type = string
+}
+ 
+variable "eks_sg_additional_rules_cidr" {
+  description = "CIDRs to allow ingress to cluster"
+  default = []
 }
